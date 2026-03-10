36a521ccaaf3014b39bfb631a620db16bf490d41
topikal009-coder <topikal009@gmail.com>
2026-03-10 09:50:08 +0200

    Change greeting from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from pyrogram.errors import PeerIdInvalid, Forbidden
 # --- КОНФИГ ---
 API_ID = 30032542
 API_HASH = "ce646da1307fb452305d49f9bb8751ca"
-BOT_TOKEN = "8711240311:AAHy5FzxQ7P0MpSm3Bv7xfoYDa9kVlwAb5w"
+import os
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8711240311:AAHy5FzxQ7P@MpSm3Bv7xfoVDa9kVIwAb5w")
 
 # === НАСТРОЙКА ОДНОРАЗОВЫХ КЛЮЧЕЙ ===
 ONE_TIME_KEYS = {
@@ -655,4 +656,5 @@ if __name__ == "__main__":
     print(f"👥 Пользователей: {len(users_data)}")
 
     bot.run()
+
 
